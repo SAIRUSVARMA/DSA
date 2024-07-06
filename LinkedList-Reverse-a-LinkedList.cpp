3b5d678a9618aec7e077e76f3071817970de17ba
SAIRUS VARMA <32862374+SAIRUSVARMA@users.noreply.github.com>
2024-07-07 01:32:44 +0530

    Create LinkedList-Reverse-a-LinkedList.cpp

---
+++ LinkedList-Reverse-a-LinkedList.cpp
@@ -0,0 +1,38 @@
+//-------------------ITERATIVE-SOLUTION---------------
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev=NULL;
+        ListNode *curr=head;
+        ListNode *nxt;
+        while(curr!=NULL)
+        {
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        return prev;
+    }
+};
+
+//TC=O(N)
+//SC=O(1)
+
+
+//----------RECURSIVE-SOLUTION-------------------
+
+Node* reverseLinkedList(Node *head)
+{
+       if(head==NULL||head->next==NULL)
+       return head;
+
+       Node* newHead= reverseLinkedList(head->next);
+       Node* front= head->next;
+       front->next=head;
+       head->next=NULL;
+       return newHead;
+}
+
+//TC=O(N)
+//SC=O(N)[For recursive call stack]
