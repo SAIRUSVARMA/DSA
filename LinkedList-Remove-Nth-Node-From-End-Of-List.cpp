36b81b213bb9dc4269be500468afabfd24e01af6
SAIRUS VARMA <32862374+SAIRUSVARMA@users.noreply.github.com>
2024-08-12 10:56:16 +0530

    Create LinkedList-Remove-Nth-Node-From-End-Of-List.cpp

---
+++ LinkedList-Remove-Nth-Node-From-End-Of-List.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy= new ListNode();
+        dummy->next=head;
+        ListNode* slow=dummy;
+        ListNode* fast=dummy;
+        for(int i=0;i<n;i++)
+        fast=fast->next;
+        while(fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next;
+    
+        }
+        slow->next=slow->next->next;
+        return dummy->next;
+    }
+};
+//TC:O(N);
+//SC:O(1);
