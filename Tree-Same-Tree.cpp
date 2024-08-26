9f51efd9451d5be55026f154e2d2a2a2d58a9b71
SAIRUS VARMA <32862374+SAIRUSVARMA@users.noreply.github.com>
2024-08-26 11:19:29 +0530

    Create Tree-Same-Tree.cpp

---
+++ Tree-Same-Tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL)
+        return true;
+
+        if(p==NULL || q==NULL)
+        return false;
+
+        if(p->val!=q->val)
+        return false;
+
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
+//TC:O(N)
+//SC:O(H)[H is the height of tree]
+//Problem:https://leetcode.com/problems/same-tree/
