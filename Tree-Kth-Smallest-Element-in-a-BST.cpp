5ba7d8f6c0c73c199c228a818b8f8cc0cc1a3f31
SAIRUS VARMA <32862374+SAIRUSVARMA@users.noreply.github.com>
2024-09-22 03:32:19 +0530

    Update Tree-Kth-Smallest-Element-in-a-BST.cpp

--- Tree-Kth-Smallest-Element-in-a-BST.cpp
+++ Tree-Kth-Smallest-Element-in-a-BST.cpp
@@ -19,4 +19,55 @@ public:
 };
 //TC:O(N)
 //SC:O(H)[O(logN) best case : Balanced BST, O(N) worst case: skewed BST]
+----------------------------------------------------------------------------------MORRIS-TRAVERSAL--------------------------
+class Solution {
+public:
+
+    int kthSmallest(TreeNode* root, int k) {
+        int ans;
+        int count=0;
+        TreeNode* curr=root;
+        while(curr)
+        {
+            //case-1
+            if(curr->left==NULL)
+            {
+                count++;
+                if(count==k)
+                ans=curr->val;
+                curr=curr->right;
+            }
+            else
+            {
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right!=curr)
+                {
+                    prev=prev->right;
+                }
+
+                if(prev->right==NULL)
+                {
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+
+                else
+                {
+                    prev->right=NULL;
+                    count++;
+                    if(count==k)
+                    ans=curr->val;
+                    curr=curr->right;
+                }
+
+                
+            }
+        }
+        return ans;
+    }
+
+   
+};  
+//TC:O(N)
+//SC:O(1)
 //Problem:https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
