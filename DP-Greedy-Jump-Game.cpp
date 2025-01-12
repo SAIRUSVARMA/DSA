8386892de37cf22ef952f439d2fe8214abf4370d
SAIRUS VARMA <32862374+SAIRUSVARMA@users.noreply.github.com>
2025-01-12 19:02:33 +0530

    Update DP-Greedy-Jump-Game.cpp

--- DP-Greedy-Jump-Game.cpp
+++ DP-Greedy-Jump-Game.cpp
@@ -14,23 +14,23 @@ public:
 //Greedy Algorithm
 //TC:O(N)
 //SC:O(1)
+
 //.....................................................................................................................
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<bool> dp(nums.size(), false);
-        dp[0] = true;
-
-        for (int i = 0; i < nums.size(); i++) 
+      vector<bool>dp(nums.size(),false);
+      dp[0]=true;
+      for(int i=0;i<nums.size();i++)
+      {
+        if(!dp[i]) continue;
+        for(int j=1;j<=nums[i] && i+j<nums.size();j++)
         {
-            if (!dp[i]) continue;
-            for (int j = 1; j <= nums[i] && i + j < nums.size(); j++) 
-            {
-                dp[i + j] = true;
-            }
+            dp[i+j]=true;
         }
-
-        return dp.back();
+      }
+      return dp[nums.size()-1];
     }
 };
 
