66972595a14c8d20fa25d46ef423f60fbb31ecf5
SAIRUS VARMA <32862374+SAIRUSVARMA@users.noreply.github.com>
2025-02-02 18:35:15 +0530

    Create Sliding-Window-Binary-Subarrays-With-Sum.cpp

---
+++ Sliding-Window-Binary-Subarrays-With-Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int sum=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            //if(mp[sum-goal]) (else would be mp[sum-goal]=0, so count+0 will still be count)
+            count+=mp[sum-goal];
+            mp[sum]++;
+        }
+        return count;
+    }
+};
+
+//TC:O(N)
+//SC:O(N)
